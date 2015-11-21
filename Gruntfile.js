f3ad238c5929ab48da99fa03bd13f622ccc0ff35
Amanda <amanda.iaria@gmail.com>
2015-11-21 16:14:56 -0500

    Set Up grunt site.
    Will be changing the domain to my portfoilo’s domain. But for now

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Show elapsed time after tasks run to visualize performance
+    require('time-grunt')(grunt);
+    // Load all Grunt tasks that are listed in package.json automagically
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // shell commands for use in Grunt tasks
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+
+        // watch for files to change and run tasks when they do
+       watch: {
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass']
+            },
+            jekyll: {
+                files: ['_layouts/*.html', '_includes/*.md', 'css/app.css'],
+                tasks: ['jekyll']
+            }
+        },
+
+        browser_sync: {
+            files: {
+                src : ['_site/css/*.css']
+            },
+            options: {
+                watchTask: true,
+                ghostMode: {
+                    clicks: true,
+                    scroll: true,
+                    links: true,
+                    forms: true
+                },
+                server: {
+                    baseDir: '_site'
+                }
+            }
+        },
+
+
+        // sass (libsass) config
+        sass: {
+            options: {
+                sourceMap: true,
+                relativeAssets: false,
+                outputStyle: 'expanded',
+                sassDir: '_sass',
+                cssDir: '_site/css'
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: '_sass/',
+                    src: ['**/*.{scss,sass}'],
+                    dest: '_site/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        // run tasks in parallel
+        concurrent: {
+            serve: [
+                'sass',
+                'watch',
+                'shell:jekyllServe'
+            ],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+
+    });
+
+    // Register the grunt serve task
+    grunt.registerTask('serve', [
+        'concurrent:serve'
+    ]);
+
+    // Register the grunt build task
+    grunt.registerTask('build', [
+        'shell:jekyllBuild',
+        'sass'
+    ]);
+
+    // Register build as the default task fallback
+    grunt.registerTask('default', 'build');
+
+};
